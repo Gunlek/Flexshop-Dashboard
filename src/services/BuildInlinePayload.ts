e7a8e2859d9e320d047c85ab3d3d5fca8d831f3b
Fabien Aubret <fabien.aubret@gmail.com>
2021-01-23 19:38:53 +0100

    :recycle: (BuildInlinePayload) Remove useless logging

--- src/services/BuildInlinePayload.ts
+++ src/services/BuildInlinePayload.ts
@@ -1,6 +1,4 @@
 export const buildInlinePayload = (payload: any) => {
-    console.log(payload.machine_title);
-    console.log("title", payload["machine_title"]);
     const payloadReducer = (accumulator: string, currentKey: string) => accumulator + currentKey + "=" + payload[currentKey] + "&";
     const inlinePayload = Object.keys(payload).reduce(payloadReducer, "");
     return inlinePayload.slice(0, inlinePayload.length-1);
