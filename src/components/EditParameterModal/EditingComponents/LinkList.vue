442487e2fa93ed1813d70580dd7322198082a3a6
Fabien Aubret <fabien.aubret@gmail.com>
2021-01-24 12:30:21 +0100

    :sparkles: (Edition) Add editing components

---
+++ src/components/EditParameterModal/EditingComponents/LinkList.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <input type="hidden" :name="name" :value="list_value" class="edition_input"/>
+        <div class="row">
+            <div class="col-8" style="padding-right: 5px;">
+                <input type="text" v-model="new_text" class="form-control content-fluid" style="margin: 0;" placeholder="Ajouter à la liste..." />
+            </div>
+            <div class="col-4" style="padding-left: 5px;">
+                <button class="content-fluid btn btn-outline-green" @click="insert_value()">Ajouter</button>
+            </div>
+        </div>
+        <div>
+            <ul v-for="(text, index) in text_list" :key="index">
+                <li>
+                    <a v-bind:href="text" target="_blank">{{text}}</a>
+                    &nbsp;&nbsp;<button class="btn btn-outline-red" @click="remove_value(index)"><VueFeather type="trash" /></button>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Prop, Component, Vue } from 'vue-property-decorator';
+    import VueFeather from 'vue-feather';
+
+    @Component({
+        components: {
+            VueFeather
+        }
+    })
+    export default class LinkList extends Vue {
+
+        @Prop({default: ""})
+        name: string;
+
+        @Prop({default: ""})
+        value: string;
+
+        new_text = "";
+        text_list: string[] = [];
+        list_value = "";
+
+        mounted(){
+            this.list_value = this.value;
+            this.text_list = this.list_value.split(';');
+        }
+
+        insert_value(){
+            if(this.new_text != ""){
+                this.text_list.push(this.new_text);
+                this.new_text = "";
+
+                this.list_value = "";
+                for(let k=0; k < this.text_list.length; k++){
+                    if(k < this.text_list.length - 1) {
+                        this.list_value += this.text_list[k];
+                        this.list_value += ";";
+                    }
+                    else
+                        this.list_value += this.text_list[k];
+                }
+            }
+        }
+
+        remove_value(index: number){
+            this.text_list.splice(index, 1);
+            
+            this.list_value = "";
+            for(let k=0; k < this.text_list.length; k++){
+                if(k < this.text_list.length - 1) {
+                    this.list_value += this.text_list[k];
+                    this.list_value += ";";
+                }
+                else
+                    this.list_value += this.text_list[k];
+            }
+        }
+    }
+</script>
---
+++ src/components/EditParameterModal/EditingComponents/PictoList.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <input type="hidden" :name="name" :value="picto_str" class="edition_input" />
+        <span v-for="(picto, index) in pictograms" :key="index">
+            <input v-model="picto_list" :checked="checked(picto)" @change="update_list()" type="checkbox" name="pictos" :value="picto" :id="picto" />
+            <label :for="picto">
+                <img :src="picto != '' && require('@/assets/img/pictograms/' + picto)" style="width: 30px; vertical-align: middle;"/>
+            </label>
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Prop, Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class PictoList extends Vue {
+
+        @Prop({default: ""})
+        name: string;
+
+        @Prop({default: ""})
+        value: string;
+        
+        pictograms: string[] = ["picto_general.png", "picto_glasses.png", "picto_gloves.png", "picto_harness.png", "picto_helmet.png", "picto_mask.png", "picto_noise.png", "picto_notice.png", "picto_pedestrian.png", "picto_shoes.png", "picto_suit.png", "picto_visor.png"];
+        picto_list: string[] = [];
+        picto_str = "";
+
+        mounted(){
+            this.picto_str = this.value;
+            this.picto_list = this.picto_str.split(';');
+        }
+
+        update_list(){
+            this.picto_str = "";
+            for(let k = 0; k < this.picto_list.length; k++){
+                if(k < this.picto_list.length - 1){
+                    this.picto_str += this.picto_list[k];
+                    this.picto_str += ";";
+                }
+                else
+                    this.picto_str += this.picto_list[k];
+            }
+        }
+
+        checked(picto: string){
+            if(this.picto_list.indexOf(picto) >= 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+</script>
