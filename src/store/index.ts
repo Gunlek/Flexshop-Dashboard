2432566c557ad9e5aaecade4b782db5946c52e43
Fabien Aubret <fabien.aubret@gmail.com>
2021-01-25 18:28:53 +0100

    :recycle: (Store) Switch actions and mutations to external files

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,7 @@
-import { getMachineSections } from '@/components/MachineCard/functions/getMachineSections';
 import { Category, Machine, Parameter, Section, Tutorial, Workshop } from '@/services/Types';
-import { getCategoryList } from '@/views/functions/getCategoryList';
-import { getMachineList } from '@/views/functions/getMachineList';
-import { getWorkshopList } from '@/views/functions/getWorkshopList';
 import Vue from 'vue'
 import Vuex from 'vuex'
+import mutations from './mutations';
 
 import actions from './actions';
 
@@ -31,94 +28,6 @@ export default new Vuex.Store({
     edition: false,
     tutorials: [] as Tutorial[]
   },
-  mutations: {
-    setDeletion(state, val): void {
-      state.enableDelete = val;
-    },
-    
-    setAddition(state, val): void {
-      state.enableAdding = val;
-    },
-
-    async getWorkshops(state): Promise<void> {
-      state.workshops = await getWorkshopList();
-    },
-
-    async getCategoriesMachines(state): Promise<void> {
-      const categoriesMachines: {
-        [categoryId: number]: Machine[];
-      } = {};
-      const promise = new Promise(resolve => {
-        state.machines.map((machine: Machine, index) => {
-          if(machine.machine_category in categoriesMachines)
-            categoriesMachines[machine.machine_category].push(machine);
-          else
-            categoriesMachines[machine.machine_category] = [machine];
-
-          if(index == state.machines.length - 1)
-            resolve(categoriesMachines);
-        });
-      });
-
-      await promise;
-      state.categoriesMachines = categoriesMachines;
-    },
-
-    async getWorkshopsCategories(state): Promise<void> {
-      const workshopsCategories: {
-        [workshopId: number]: Category[];
-      } = {};
-      const promise = new Promise(resolve => {
-        state.categories.map((category: Category, index) => {
-          if(category.category_workshop in workshopsCategories)
-            workshopsCategories[category.category_workshop].push(category);
-          else
-            workshopsCategories[category.category_workshop] = [category];
-
-          if(index == state.categories.length - 1)
-            resolve(workshopsCategories);
-        });
-      });
-
-      await promise;
-      state.workshopsCategories = workshopsCategories;
-    },
-
-    async getCategories(state): Promise<void> {
-      state.categories = await getCategoryList();
-    },
-
-    async getMachines(state): Promise<void> {
-      state.machines = await getMachineList();
-    },
-
-    async getMachineSections(state): Promise<void> {
-      const sections = {};
-      const promises = state.machines.map((machine: Machine) => 
-        new Promise(resolve => {
-          getMachineSections(machine.machine_id)
-            .then((section) => {
-              sections[machine.machine_id] = section;
-              resolve(state.machineSections);
-            });
-        })
-      );
-      
-      await Promise.all(promises);
-      state.machineSections = sections;
-    },
-
-    setEditedSection(state, payload: {
-      section: Section;
-      parameters: Parameter[];
-    }): void {
-      state.editedSection = payload.section;
-      state.editedParameters = payload.parameters;
-    },
-
-    editionEnabled(state, enabled): void {
-      state.edition = enabled;
-    }
-  },
+  mutations,
   actions
 })
--- src/store/mutations/getMachineSections.ts
+++ src/store/mutations/getMachineSections.ts
@@ -1,11 +1,31 @@
-import { Machine } from "@/services/Types";
+import { GETRequest } from "@/services/APIRequest";
+import { Machine, Section, SectionExtended } from "@/services/Types";
+import sectionDescription from "@/components/MachineCard/functions/sections_description";
+
+const getMachineSectionsFromAPI = async (machineId: number): Promise<SectionExtended[]> => {
+    const sections: SectionExtended[] = [];
+    const requestPromise = new Promise((resolve) => {
+        GETRequest('sections/list', (status: number, results: Section[]) => {
+            results.forEach((section, index) => {
+                if(section.section_machine === machineId){
+                    sections.push({...section, section_display_name: sectionDescription[section.section_type].display_name});
+                }
+
+                if(index === results.length - 1) resolve(sections);
+            })
+        });
+    });
+
+    await Promise.all([requestPromise]);
+
+    return sections;
+}
 
 const getMachineSections = async (state): Promise<void> => {
     const sections = {};
-    console.log(state);
     const promises = state.machines.map((machine: Machine) => 
     new Promise(resolve => {
-        getMachineSections(machine.machine_id)
+        getMachineSectionsFromAPI(machine.machine_id)
         .then((section) => {
             sections[machine.machine_id] = section;
             resolve(state.machineSections);
