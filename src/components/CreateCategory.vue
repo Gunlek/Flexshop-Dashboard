a9f75a8556cdca0b34ae8a987082f44dc64f4871
Fabien Aubret <fabien.aubret@gmail.com>
2021-01-25 11:58:36 +0100

    :sparkles: (CreateCategory) Add createCategory method and component

---
+++ src/components/CreateCategory.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="card align-parent">
+        <div class="card-header align-parent">
+            <strong class="card-title">Ajouter une catégorie</strong>
+        </div>
+        <input type="text" class="form-control content-fluid" v-model="categoryTitle" placeholder="Nom de la catégorie..." />
+        <button type="button" @click="createCategory()" class="content-fluid btn btn-outline-green">Créer</button>
+    </div>
+</template>
+
+<script lang="ts">
+    import { POSTRequest } from '@/services/APIRequest';
+    import { Prop, Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class CreateCategory extends Vue {
+
+        categoryTitle = "";
+
+        @Prop()
+        workshopId: number;
+
+        createCategory(){
+            POSTRequest("category/add", {
+                category_workshop: this.workshopId,
+                category_title: this.categoryTitle
+            }, () => {
+                this.$store.dispatch("refreshCategories");
+            })
+        }
+
+    }
+</script>
